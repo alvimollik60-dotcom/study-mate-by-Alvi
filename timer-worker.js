7246a4683ab2249d10d6fcf1be89e65ca0476a9f
alvimollik60-dotcom <alvimollik60@gmail.com>
2026-06-23 10:22:45 +0600

    Add files via upload

---
+++ timer-worker.js
@@ -0,0 +1,13 @@
+// timer-worker.js
+let interval = null;
+
+self.onmessage = function(e) {
+    if (e.data === 'start') {
+        if (interval) clearInterval(interval);
+        interval = setInterval(() => {
+            self.postMessage('tick');
+        }, 1000);
+    } else if (e.data === 'stop') {
+        if (interval) clearInterval(interval);
+    }
+};
